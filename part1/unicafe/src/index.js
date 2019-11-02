a195b1d95c84b2b5e776b6721628b64774f299c3
Daniel Gawne <danielgawne@gmail.com>
2019-11-01 22:37:35 -0400

    exercise 1.6

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,10 +7,42 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGoodClick = () => {
+    setGood(good + 1);
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1);
+  }
+
   return (
     <div>
-      code here
+      <h1>Give Feedback!</h1>
+      <Button rating="good" handler={handleGoodClick}/>
+      <Button rating="neutral" handler={handleNeutralClick}/>
+      <Button rating="bad" handler={handleBadClick}/>
+      <h2>Statistics</h2>
+      <Stat rating="good" votes={good}/>
+      <Stat rating="neutral" votes={neutral}/>
+      <Stat rating="bad" votes={bad}/>
+
     </div>
+  )
+}
+
+const Stat = (props) => {
+  return (
+    <p>{props.rating} votes: {props.votes}</p>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handler}>{props.rating}</button>
   )
 }
 
