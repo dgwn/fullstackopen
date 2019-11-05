411fb683df3654bdf7003ee9443b67cdb31e164d
Daniel Gawne <danielgawne@gmail.com>
2019-11-05 16:53:31 -0500

    exercise 2.4

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -2,41 +2,67 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
 
-
+  const generateCourses = () => courses.map( currentCourse => <Course course={currentCourse}/> )
 
 
   return (
     <div>
-      <Course course={course}/>
+      {generateCourses()}
     </div>
   )
 }
 
 const Course = ({ course }) => {
+
   return (
     <div>
+      <h1>Web Dev Curriculum</h1>
       <Header course={course.name}/>
       <Content parts={course.parts}/>
     </div>
@@ -65,7 +91,7 @@ const Part = (props) => {
 
 const Header = (props) => {
   return (
-    <h1>{props.course}</h1>
+    <h2>{props.course}</h2>
   )
 }
 
