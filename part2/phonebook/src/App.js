d8887567a793116af2640992a300d50260bd5cf9
Daniel Gawne <danielgawne@gmail.com>
2019-11-06 19:12:14 -0500

    exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,20 +3,24 @@ import React, { useState } from 'react'
 const App = () => {
   const [ persons, setPersons] = useState([
     { name: 'Arto Hellas',
-      id: 0
+      id: 0,
+      number: '555-5555'
     }
   ])
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
     const nameObject = {
       name: newName,
-      id: (persons.length)
+      id: (persons.length),
+      number: newNumber
     }
     if (checkNames() === true) {
       setPersons(persons.concat(nameObject))
       setNewName('')
+      setNewNumber('')
     }
     else {window.alert(`"${nameObject.name}" is already in the phonebook`)}
 
@@ -26,11 +30,15 @@ const App = () => {
     setNewName(event.target.value)
   }
 
-  const displayNames = () => persons.map( person => <li id={person.id}>{person.name}</li>)
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const displayNames = () => persons.map( person => <li id={person.id}>{person.name}: {person.number}</li>)
 
   const checkNames = () => {
     for (let i =0; i < persons.length; i++) {
-      if (newName === persons[i].name) {
+      if (newName.toLowerCase() === persons[i].name.toLowerCase()) {
         return false
       }
     }
@@ -45,6 +53,8 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
+        <div/>
           <button type="submit" onClick={addName}>add</button>
         </div>
         {/* <div>debug: {}</div> */}
