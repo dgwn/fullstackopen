80181e5e303c73aa0e0ce7c59fddc3901b9bc7dd
Daniel Gawne <danielgawne@gmail.com>
2020-08-16 22:34:52 -0400

    exercise 4.3

---
+++ part4/utils/list_helper.js
@@ -0,0 +1,16 @@
+// 4.3 dummy function
+const dummy = () => {
+  return 1;
+};
+
+// 4.4 totalLikes function
+
+// 4.5 favoriteBlog function
+
+// 4.6 mostBlogs function
+
+// 4.7 mostLikes function
+
+module.exports = {
+  dummy
+};
