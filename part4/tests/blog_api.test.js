62be46084fe600814a773bc7fb0bfa7a14eeb03b
Daniel Gawne <danielgawne@gmail.com>
2020-08-23 20:37:57 -0400

    exercise 4.10

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -49,9 +49,31 @@ test("there are 2 blogs", async () => {
 test("the blog has the property 'id'", async () => {
   const response = await api.get("/api/blogs");
   for (const blog of response.body) {
-    console.log(blog);
     expect(blog.id).toBeDefined();
   }
+});
+
+test("successful new blog POST", async () => {
+  const newBlog = {
+    id: "5f4302d0ce01de27c3caaa94",
+    title: "Wikipedia",
+    author: "Jimmy Wales",
+    url: "http://www.wikipedia.org",
+    likes: 6,
+    __v: 0
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const response = await api.get("/api/blogs");
+  expect(response.body).toHaveLength(initialBlogs.length + 1);
+
+  const titles = response.body.map((n) => n.title);
+  expect(titles).toContain(newBlog.title);
 });
 
 afterAll(() => {
