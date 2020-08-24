77337a5e77479cbdc0d011b27a8141cd6cff8561
Daniel Gawne <danielgawne@gmail.com>
2020-08-23 20:14:31 -0400

    exercise 4.9

--- part4/models/blog.js
+++ part4/models/blog.js
@@ -3,20 +3,27 @@ const mongoose = require("mongoose");
 const blogSchema = mongoose.Schema({
   title: {
     type: String,
-    required: true,
+    required: true
   },
   author: {
     type: String,
-    required: true,
+    required: true
   },
   url: {
     type: String,
-    required: true,
+    required: true
   },
   likes: {
     type: Number,
-    required: true,
-  },
+    required: true
+  }
+});
+
+blogSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+  }
 });
 
 module.exports = mongoose.model("Blog", blogSchema);
--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,11 +1,37 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
-
 const api = supertest(app);
+const Blog = require("../models/blog");
 
-beforeEach(() => {
-  jest.setTimeout(30000);
+const initialBlogs = [
+  {
+    id: "5f4302d0ce01de27c3caaa9d",
+    title: "Google",
+    author: "Larry Page",
+    url: "http://www.google.com",
+    likes: 5,
+    __v: 0
+  },
+  {
+    id: "5f4302d0ce01de27c3caaa93",
+    title: "Facebook",
+    author: "Mark Zuck",
+    url: "http://www.facebook.com",
+    likes: 0,
+    __v: 0
+  }
+];
+
+beforeEach(async () => {
+  await jest.setTimeout(30000);
+  await Blog.deleteMany({});
+
+  let blogObject = new Blog(initialBlogs[0]);
+  await blogObject.save();
+
+  blogObject = new Blog(initialBlogs[1]);
+  await blogObject.save();
 });
 
 test("blogs are returned as json", async () => {
@@ -15,9 +41,17 @@ test("blogs are returned as json", async () => {
     .expect("Content-Type", /application\/json/);
 });
 
-test("there is one blog", async () => {
+test("there are 2 blogs", async () => {
   const response = await api.get("/api/blogs");
-  expect(response.body).toHaveLength(1);
+  expect(response.body).toHaveLength(initialBlogs.length);
+});
+
+test("the blog has the property 'id'", async () => {
+  const response = await api.get("/api/blogs");
+  for (const blog of response.body) {
+    console.log(blog);
+    expect(blog.id).toBeDefined();
+  }
 });
 
 afterAll(() => {
