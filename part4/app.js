513cea2435414cd71974fd23908c6783b0b5d26b
Daniel Gawne <danielgawne@gmail.com>
2020-09-04 00:07:26 -0400

    require and use users controller

--- part4/app.js
+++ part4/app.js
@@ -4,6 +4,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
 const mongoose = require("mongoose");
 
 const url = config.MONGODB_URI;
@@ -23,5 +24,6 @@ mongoose
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 
 module.exports = app;
