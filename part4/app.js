d631dd135af003cca5bf0ca40dac5285a162e3fc
Daniel Gawne <danielgawne@gmail.com>
2020-09-11 23:44:22 -0400

    exercise 4.18

--- part4/app.js
+++ part4/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const loginRouter = require("./controllers/login");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
 const mongoose = require("mongoose");
@@ -25,5 +26,6 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
 module.exports = app;
