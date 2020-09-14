1bf204228080f310681cd889096c2c664af50863
Daniel Gawne <danielgawne@gmail.com>
2020-09-14 00:24:26 -0400

    exercise 4.20

--- part4/app.js
+++ part4/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const middleware = require("./utils/middleware");
 const loginRouter = require("./controllers/login");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
@@ -24,6 +25,7 @@ mongoose
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(middleware.tokenExtractor);
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -4,14 +4,6 @@ const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
 
-const getTokenFrom = (request) => {
-  const authorization = request.get("authorization");
-  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
-    return authorization.substring(7);
-  }
-  return null;
-};
-
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", {
     username: 1,
@@ -47,9 +39,8 @@ blogsRouter.post("/", async (request, response) => {
   try {
     const body = request.body;
 
-    const token = getTokenFrom(request);
-    const decodedToken = jwt.verify(token, process.env.SECRET);
-    if (!token || !decodedToken.id) {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET);
+    if (!request.token || !decodedToken.id) {
       return response.status(401).json({ error: "token missing or invalid" });
     }
 
