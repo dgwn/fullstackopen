c19f6926a02afc2da54d55f9a66348e2e420a50d
Daniel Gawne <danielgawne@gmail.com>
2020-10-30 19:36:13 -0400

    remove notification style remnants in favor of material-ui Alerts

--- part5/src/components/Notification.js
+++ part5/src/components/Notification.js
@@ -2,15 +2,6 @@ import React from "react";
 import Alert from "@material-ui/lab/Alert";
 
 const notificationAlert = ({ notification }) => {
-  const notificationStyle = {
-    color: "black",
-    backgroundColor: "grey",
-    margin: "auto",
-    width: "50%",
-    textAlign: "center",
-    fontSize: "26px"
-  };
-
   return (
     <div>
       <Alert severity="info">{notification}</Alert>
