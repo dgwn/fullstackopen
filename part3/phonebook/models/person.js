834b0a1ffad86d2d785753356a6237ab5d4fc52f
Daniel Gawne <danielgawne@gmail.com>
2021-11-16 22:32:28 -0500

    patch dependencies in server

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose')
-const uniqueValidator = require('mongoose-unique-validator')
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 // You will need to add the password in the .env file! It has been removed for securtiy purposes
-const url = process.env.MONGODB_URI
+const url = process.env.MONGODB_URI;
 
 // fix deprecation warning for findAndModify
-mongoose.set('useFindAndModify', false)
 
-mongoose.connect(url, {useNewUrlParser: true})
-  .then(result => {
-    console.log('connected to MongoDB')
+mongoose
+  .connect(url, { useNewUrlParser: true })
+  .then((result) => {
+    console.log("connected to MongoDB");
   })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
 
 // validation: person names must be unique, name must be at least 3char and number must be at least 8 digits
 
@@ -29,15 +29,15 @@ const personSchema = new mongoose.Schema({
     minlength: 8
   },
   id: String
-})
+});
 
 //removes _v and converts _id to string
-personSchema.set('toJSON', {
+personSchema.set("toJSON", {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
   }
-})
+});
 
-module.exports = mongoose.model('Person', personSchema)
+module.exports = mongoose.model("Person", personSchema);
